380e66708537db8df85c11a9aa1779ea7562adc8
chauhungphong2020 <chauhungphong2020@gmail.com>
2026-05-11 15:18:21 +0700

    Update main.m

--- main.m
+++ main.m
@@ -1,26 +1,20 @@
 #import <UIKit/UIKit.h>
-#import <Security/Security.h>
+#import <objc/runtime.h>
 
-@interface WipeController : NSObject
-+ (void)doWipe;
-@end
+@implementation NSObject (WipeHack)
 
-@implementation WipeController
-+ (void)doWipe {
-    // 1. Xóa sạch Keychain (ID định danh nằm ở đây)
-    NSArray *secClasses = @[
-        (__bridge id)kSecClassGenericPassword,
-        (__bridge id)kSecClassInternetPassword,
-        (__bridge id)kSecClassCertificate,
-        (__bridge id)kSecClassKey,
-        (__bridge id)kSecClassIdentity
-    ];
-    for (id secClass in secClasses) {
-        NSDictionary *spec = @{(__bridge id)kSecClass: secClass};
-        SecItemDelete((__bridge CFDictionaryRef)spec);
+// Hàm tạo chuỗi ngẫu nhiên để làm ID mới
++ (NSString *)randomID {
+    static NSString *letters = @"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    NSMutableString *randomString = [NSMutableString stringWithCapacity:32];
+    for (int i=0; i<32; i++) {
+        [randomString appendFormat: @"%C", [letters characterAtIndex: arc4random_uniform((uint32_t)[letters length])]];
     }
+    return randomString;
+}
 
-    // 2. Xóa sạch file trong Documents, Library, tmp
++ (void)doWipe {
+    // 1. Xóa sạch rác trong máy
     NSString *home = NSHomeDirectory();
     NSFileManager *fm = [NSFileManager defaultManager];
     NSArray *folders = @[@"Documents", @"Library", @"tmp"];
@@ -30,9 +24,12 @@
         [fm createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:nil];
     }
 
-    // 3. Reset UserDefaults
-    [[NSUserDefaults standardUserDefaults] removePersistentDomainForName:[[NSBundle mainBundle] bundleIdentifier]];
-    [[NSUserDefaults standardUserDefaults] synchronize];
+    // 2. Ép App tạo ID mới bằng cách ghi đè trực tiếp vào bộ nhớ tạm
+    NSUserDefaults *defs = [NSUserDefaults standardUserDefaults];
+    [defs setObject:[self randomID] forKey:@"stk_idfv_key"];
+    [defs setObject:[self randomID] forKey:@"userID"];
+    [defs setObject:[self randomID] forKey:@"uuidStringFromStore"];
+    [defs synchronize];
 
     exit(0);
 }
@@ -42,22 +39,16 @@ __attribute__((constructor))
 static void init() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-        
-        // Căn giữa màn hình
         CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
-        CGFloat btnWidth = 140;
-        btn.frame = CGRectMake((screenWidth - btnWidth) / 2, 60, btnWidth, 45); 
+        btn.frame = CGRectMake((screenWidth - 150) / 2, 60, 150, 45); 
         
-        [btn setTitle:@"WIPE & RESET" forState:UIControlStateNormal];
-        [btn setBackgroundColor:[UIColor colorWithRed:1.0 green:0.2 blue:0.2 alpha:0.9]];
+        [btn setTitle:@"FORCE NEW ID" forState:UIControlStateNormal];
+        [btn setBackgroundColor:[UIColor colorWithRed:0.0 green:0.6 blue:0.2 alpha:0.9]];
         [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         btn.layer.cornerRadius = 12;
         btn.layer.zPosition = 9999;
         
-        [btn addTarget:[WipeController class] action:@selector(doWipe) forControlEvents:UIControlEventTouchUpInside];
-        
-        // Thêm vào cửa sổ chính
-        UIWindow *window = [UIApplication sharedApplication].keyWindow;
-        [window addSubview:btn];
+        [btn addTarget:[NSObject class] action:@selector(doWipe) forControlEvents:UIControlEventTouchUpInside];
+        [[UIApplication sharedApplication].keyWindow addSubview:btn];
     });
 }
