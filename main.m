7a7d7261a1e4c627128ff0777914f74b8e76d32a
chauhungphong2020 <chauhungphong2020@gmail.com>
2026-05-11 15:09:50 +0700

    Update main.m

--- main.m
+++ main.m
@@ -7,17 +7,7 @@
 
 @implementation WipeController
 + (void)doWipe {
-    // 1. Xoá sạch file trong Documents, Library, tmp
-    NSString *home = NSHomeDirectory();
-    NSFileManager *fm = [NSFileManager defaultManager];
-    NSArray *folders = @[@"Documents", @"Library", @"tmp"];
-    for (NSString *folder in folders) {
-        NSString *path = [home stringByAppendingPathComponent:folder];
-        [fm removeItemAtPath:path error:nil];
-        [fm createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:nil];
-    }
-
-    // 2. Xoá Keychain (Đây là chìa khoá để hồi Credit)
+    // 1. Xóa sạch Keychain (ID định danh nằm ở đây)
     NSArray *secClasses = @[
         (__bridge id)kSecClassGenericPassword,
         (__bridge id)kSecClassInternetPassword,
@@ -30,11 +20,20 @@
         SecItemDelete((__bridge CFDictionaryRef)spec);
     }
 
-    // 3. Xoá NSUserDefaults
+    // 2. Xóa sạch file trong Documents, Library, tmp
+    NSString *home = NSHomeDirectory();
+    NSFileManager *fm = [NSFileManager defaultManager];
+    NSArray *folders = @[@"Documents", @"Library", @"tmp"];
+    for (NSString *folder in folders) {
+        NSString *path = [home stringByAppendingPathComponent:folder];
+        [fm removeItemAtPath:path error:nil];
+        [fm createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:nil];
+    }
+
+    // 3. Reset UserDefaults
     [[NSUserDefaults standardUserDefaults] removePersistentDomainForName:[[NSBundle mainBundle] bundleIdentifier]];
     [[NSUserDefaults standardUserDefaults] synchronize];
 
-    // Thoát app
     exit(0);
 }
 @end
@@ -43,18 +42,22 @@ __attribute__((constructor))
 static void init() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
+        
+        // Căn giữa màn hình
         CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+        CGFloat btnWidth = 140;
+        btn.frame = CGRectMake((screenWidth - btnWidth) / 2, 60, btnWidth, 45); 
         
-        // Vị trí: Chính giữa phía trên (cách đỉnh 60px)
-        btn.frame = CGRectMake((screenWidth - 140) / 2, 60, 140, 45); 
-        
-        [btn setTitle:@"CLEAR ALL DATA" forState:UIControlStateNormal];
+        [btn setTitle:@"WIPE & RESET" forState:UIControlStateNormal];
         [btn setBackgroundColor:[UIColor colorWithRed:1.0 green:0.2 blue:0.2 alpha:0.9]];
         [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
         btn.layer.cornerRadius = 12;
         btn.layer.zPosition = 9999;
         
         [btn addTarget:[WipeController class] action:@selector(doWipe) forControlEvents:UIControlEventTouchUpInside];
-        [[UIApplication sharedApplication].keyWindow addSubview:btn];
+        
+        // Thêm vào cửa sổ chính
+        UIWindow *window = [UIApplication sharedApplication].keyWindow;
+        [window addSubview:btn];
     });
 }
