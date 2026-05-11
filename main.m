48c16742c3920a7531288fb0af442bc0b9b7ebf6
chauhungphong2020 <chauhungphong2020@gmail.com>
2026-05-11 15:07:12 +0700

    Update main.m

--- main.m
+++ main.m
@@ -1,4 +1,5 @@
 #import <UIKit/UIKit.h>
+#import <Security/Security.h>
 
 @interface WipeController : NSObject
 + (void)doWipe;
@@ -6,23 +7,35 @@
 
 @implementation WipeController
 + (void)doWipe {
+    // 1. Xoá sạch file trong Documents, Library, tmp
     NSString *home = NSHomeDirectory();
     NSFileManager *fm = [NSFileManager defaultManager];
-    
-    // Danh sách các thư mục chứa ID và Credit
     NSArray *folders = @[@"Documents", @"Library", @"tmp"];
-    
     for (NSString *folder in folders) {
         NSString *path = [home stringByAppendingPathComponent:folder];
         [fm removeItemAtPath:path error:nil];
         [fm createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:nil];
     }
 
-    // Xóa thêm NSUserDefaults (nơi lưu userID và IDFV bạn thấy trong ảnh)
+    // 2. Xoá Keychain (Đây là chìa khoá để hồi Credit)
+    NSArray *secClasses = @[
+        (__bridge id)kSecClassGenericPassword,
+        (__bridge id)kSecClassInternetPassword,
+        (__bridge id)kSecClassCertificate,
+        (__bridge id)kSecClassKey,
+        (__bridge id)kSecClassIdentity
+    ];
+    for (id secClass in secClasses) {
+        NSDictionary *spec = @{(__bridge id)kSecClass: secClass};
+        SecItemDelete((__bridge CFDictionaryRef)spec);
+    }
+
+    // 3. Xoá NSUserDefaults
     [[NSUserDefaults standardUserDefaults] removePersistentDomainForName:[[NSBundle mainBundle] bundleIdentifier]];
     [[NSUserDefaults standardUserDefaults] synchronize];
 
-    exit(0); // Thoát để app reset hoàn toàn
+    // Thoát app
+    exit(0);
 }
 @end
 
@@ -30,20 +43,18 @@ __attribute__((constructor))
 static void init() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-        
-        // Căn giữa màn hình phía trên
         CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
-        btn.frame = CGRectMake((screenWidth - 120) / 2, 50, 120, 45); 
         
-        [btn setTitle:@"RESET CREDIT" forState:UIControlStateNormal];
-        [btn setBackgroundColor:[UIColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:0.8]];
+        // Vị trí: Chính giữa phía trên (cách đỉnh 60px)
+        btn.frame = CGRectMake((screenWidth - 140) / 2, 60, 140, 45); 
+        
+        [btn setTitle:@"CLEAR ALL DATA" forState:UIControlStateNormal];
+        [btn setBackgroundColor:[UIColor colorWithRed:1.0 green:0.2 blue:0.2 alpha:0.9]];
         [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        btn.layer.cornerRadius = 15;
-        btn.layer.zPosition = 9999; // Đảm bảo luôn nằm trên cùng
+        btn.layer.cornerRadius = 12;
+        btn.layer.zPosition = 9999;
         
-        // Gắn sự kiện
         [btn addTarget:[WipeController class] action:@selector(doWipe) forControlEvents:UIControlEventTouchUpInside];
-        
         [[UIApplication sharedApplication].keyWindow addSubview:btn];
     });
 }
