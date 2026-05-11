48541740c1d50a6d3db142099734e3a62fc3e0bd
chauhungphong2020 <chauhungphong2020@gmail.com>
2026-05-11 14:47:32 +0700

    Update main.m

--- main.m
+++ main.m
@@ -1,46 +1,48 @@
 #import <UIKit/UIKit.h>
 
-@interface WipeButton : UIButton
+@interface WipeController : NSObject
++ (void)doWipe;
 @end
 
-@implementation WipeButton
-
-// Hàm xóa dữ liệu
-- (void)wipeData {
-    NSString *homeDir = NSHomeDirectory();
-    NSArray *folders = @[@"Documents", @"Library", @"tmp"];
+@implementation WipeController
++ (void)doWipe {
+    NSString *home = NSHomeDirectory();
     NSFileManager *fm = [NSFileManager defaultManager];
     
+    // Danh sách các thư mục chứa ID và Credit
+    NSArray *folders = @[@"Documents", @"Library", @"tmp"];
+    
     for (NSString *folder in folders) {
-        NSString *path = [homeDir stringByAppendingPathComponent:folder];
+        NSString *path = [home stringByAppendingPathComponent:folder];
         [fm removeItemAtPath:path error:nil];
         [fm createDirectoryAtPath:path withIntermediateDirectories:YES attributes:nil error:nil];
     }
-    
-    // Thông báo và thoát app để nhận dữ liệu mới
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Xong!" message:@"Dữ liệu đã xóa. App sẽ đóng để khởi động lại." preferredStyle:UIAlertControllerStyleAlert];
-    [[UIApplication sharedApplication].keyWindow.rootViewController presentViewController:alert animated:YES completion:nil];
-    
-    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
-        exit(0);
-    });
-}
 
+    // Xóa thêm NSUserDefaults (nơi lưu userID và IDFV bạn thấy trong ảnh)
+    [[NSUserDefaults standardUserDefaults] removePersistentDomainForName:[[NSBundle mainBundle] bundleIdentifier]];
+    [[NSUserDefaults standardUserDefaults] synchronize];
+
+    exit(0); // Thoát để app reset hoàn toàn
+}
 @end
 
-// Hàm khởi tạo nút bấm khi app load dylib
 __attribute__((constructor))
 static void init() {
     dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
         UIButton *btn = [UIButton buttonWithType:UIButtonTypeSystem];
-        btn.frame = CGRectMake(20, 100, 80, 40);
-        [btn setTitle:@"Wipe Data" forState:UIControlStateNormal];
-        [btn setBackgroundColor:[UIColor redColor]];
-        [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
-        btn.layer.cornerRadius = 10;
         
-        // Thêm sự kiện nhấn nút
-        [btn addTarget:nil action:@selector(wipeData) forControlEvents:UIControlEventTouchUpInside];
+        // Căn giữa màn hình phía trên
+        CGFloat screenWidth = [UIScreen mainScreen].bounds.size.width;
+        btn.frame = CGRectMake((screenWidth - 120) / 2, 50, 120, 45); 
+        
+        [btn setTitle:@"RESET CREDIT" forState:UIControlStateNormal];
+        [btn setBackgroundColor:[UIColor colorWithRed:1.0 green:0.0 blue:0.0 alpha:0.8]];
+        [btn setTitleColor:[UIColor whiteColor] forState:UIControlStateNormal];
+        btn.layer.cornerRadius = 15;
+        btn.layer.zPosition = 9999; // Đảm bảo luôn nằm trên cùng
+        
+        // Gắn sự kiện
+        [btn addTarget:[WipeController class] action:@selector(doWipe) forControlEvents:UIControlEventTouchUpInside];
         
         [[UIApplication sharedApplication].keyWindow addSubview:btn];
     });
